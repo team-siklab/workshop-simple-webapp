39de31757acf2045e4939fde6bef4164e2eb510a
Richard Neil Ilagan <me@richardneililagan.com>
2019-03-01 14:49:30 +0800

    Adds expensive computation handler

--- src/handlers/computation.js
+++ src/handlers/computation.js
@@ -1,17 +1,32 @@
-const computation = (req, res) => {
+const computation = async (req, res) => {
   const limit = 1e6
 
+  return new Promise((resolve, reject) => {
+    // :: this is a deliberately long, inefficient computation
+    let lasthash = ''
+
+    for (var i = 0; i < limit; i++) {
+      lasthash = require('crypto')
+        .createHash('md5')
+        .update(lasthash + new Date())
+        .digest('hex')
+    }
+
+    resolve(lasthash)
+    res.send(lasthash)
+  })
+
   // :: this is a deliberately long, inefficient computation
-  let lasthash = ''
+  // let lasthash = ''
 
-  for (var i = 0; i < limit; i++) {
-    lasthash = require('crypto')
-      .createHash('md5')
-      .update(lasthash + new Date())
-      .digest('hex')
-  }
+  // for (var i = 0; i < limit; i++) {
+  //   lasthash = require('crypto')
+  //     .createHash('md5')
+  //     .update(lasthash + new Date())
+  //     .digest('hex')
+  // }
 
-  res.send(lasthash)
+  // res.send(lasthash)
 }
 
 module.exports = computation
