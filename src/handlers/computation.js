36f14512190f1b6397222bff098a042e63e47b0e
Richard Neil Ilagan <me@richardneililagan.com>
2019-03-01 15:11:01 +0800

    Shifts to promise-based computation handler

--- src/handlers/computation.js
+++ src/handlers/computation.js
@@ -1,4 +1,4 @@
-const computation = async (req, res) => {
+const computation = () => {
   const limit = 1e6
 
   return new Promise((resolve, reject) => {
@@ -13,20 +13,12 @@ const computation = async (req, res) => {
     }
 
     resolve(lasthash)
-    res.send(lasthash)
   })
-
-  // :: this is a deliberately long, inefficient computation
-  // let lasthash = ''
-
-  // for (var i = 0; i < limit; i++) {
-  //   lasthash = require('crypto')
-  //     .createHash('md5')
-  //     .update(lasthash + new Date())
-  //     .digest('hex')
-  // }
-
-  // res.send(lasthash)
 }
 
-module.exports = computation
+const handler = async (req, res) => {
+  const result = await computation()
+  res.send(result)
+}
+
+module.exports = handler
